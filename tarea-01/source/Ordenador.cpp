5a4693f02af01b781dc5cf9f4904025d52c89f83
pullusdoctus <ja.rojas.dev@gmail.com>
2025-05-04 12:57:47 -0600

    [T01] Implementar Selection Sort

--- tarea-01/source/Ordenador.cpp
+++ tarea-01/source/Ordenador.cpp
@@ -3,20 +3,45 @@
 #include <iostream>
 
 void Ordenador::ordenamientoPorSeleccion(uint32_t *A, uint32_t n) const {
-  std::cout << "seleccion" << std::endl;
+  for (uint32_t i = 0; i < n; ++i) {
+  uint32_t m = i;
+    for (uint32_t j = i + 1; j <= n; ++j) {
+      if (A[j] < A[m]) {
+        m = j;
+      }
+    }
+    this->intercambiar(A, i, m);
+  }
 }
 void Ordenador::ordenamientoPorInserccion(uint32_t *A, uint32_t n) const {
+  (void)A;
+  (void)n;
   std::cout << "insercion" << std::endl;
 }
 void Ordenador::ordenamientoPorMezcla(uint32_t *A, uint32_t n) const {
+  (void)A;
+  (void)n;
   std::cout << "mezcla" << std::endl;
 }
 void Ordenador::ordenamientoPorMonticulos(uint32_t *A, uint32_t n) const {
+  (void)A;
+  (void)n;
   std::cout << "monticulos" << std::endl;
 }
 void Ordenador::ordenamientoRapido(uint32_t *A, uint32_t n) const {
+  (void)A;
+  (void)n;
   std::cout << "rapido" << std::endl;
 }
 void Ordenador::ordenamientoPorResiduos(uint32_t *A, uint32_t n) const {
+  (void)A;
+  (void)n;
   std::cout << "residuos" << std::endl;
+}
+
+// Metodos auxiliares
+void Ordenador::intercambiar(uint32_t* A, uint32_t i, uint32_t j) const {
+  uint32_t temp = A[i];
+  A[i] = A[j];
+  A[j] = temp;
 }
--- tarea-01/source/include/Ordenador.hpp
+++ tarea-01/source/include/Ordenador.hpp
@@ -11,38 +11,37 @@ Modified by: Prof. Allan Berrocal, Universidad de Costa Rica
 #include <string>
 
 class Ordenador {
-    private:
-    // Defina aqui los metodos auxiliares de los algoritmos de ordenamiento solamente.
-    // Puede definir cuantos metodos quiera.
-    
-    public:
-    Ordenador() = default;
-    ~Ordenador() = default;
-    /* Nota:
-     - Si no planea implementar algunos de los métodos de ordenamiento, no los borre.
-     - Simplemente déjelos con el cuerpo vacío para evitar errores de compilación, ya
-       que se ejecutará el mismo main para todas las tareas.
-     - Recuerde hacer uso de programación defensiva y documentar los métodos con formato Doxygen, por ejemplo.
-     - No cambié la firma de los métodos de la clase Ordenador.
-     - No lance excepciones para el manejo de errores.
-       Ante un error, basta con que el método no modifique el arreglo original y que no cause la caída del programa.
-    */ 
-  
-    void ordenamientoPorSeleccion(uint32_t *A, uint32_t n) const;
-    void ordenamientoPorInserccion(uint32_t *A, uint32_t n) const;
-    void ordenamientoPorMezcla(uint32_t *A, uint32_t n) const;
-    void ordenamientoPorMonticulos(uint32_t *A, uint32_t n) const;
-    void ordenamientoRapido(uint32_t *A, uint32_t n) const;
-    void ordenamientoPorResiduos(uint32_t *A, uint32_t n) const;
+  private:
+  // Metodos auxiliares
+  void intercambiar(uint32_t* A, uint32_t i, uint32_t j) const;
+  public:
+  Ordenador() = default;
+  ~Ordenador() = default;
+  /* Nota:
+   - Si no planea implementar algunos de los métodos de ordenamiento, no los borre.
+   - Simplemente déjelos con el cuerpo vacío para evitar errores de compilación, ya
+     que se ejecutará el mismo main para todas las tareas.
+   - Recuerde hacer uso de programación defensiva y documentar los métodos con formato Doxygen, por ejemplo.
+   - No cambié la firma de los métodos de la clase Ordenador.
+   - No lance excepciones para el manejo de errores.
+     Ante un error, basta con que el método no modifique el arreglo original y que no cause la caída del programa.
+  */ 
 
-    /**
-     * @brief Retorna un std::string con los datos de la tarea.
-     * 
-     * Este método devuelve una cadena de texto que contiene el carné, nombre y tarea.
-     * 
-     * @return std::string Una cadena de texto con los datos de la tarea.
-     */
-    constexpr const char* imprimirDatosDeTarea() const {
-      return "Carné: B96804, Nombre: Jeremy Rojas, Tarea 1";
-    }
+  void ordenamientoPorSeleccion(uint32_t *A, uint32_t n) const;
+  void ordenamientoPorInserccion(uint32_t *A, uint32_t n) const;
+  void ordenamientoPorMezcla(uint32_t *A, uint32_t n) const;
+  void ordenamientoPorMonticulos(uint32_t *A, uint32_t n) const;
+  void ordenamientoRapido(uint32_t *A, uint32_t n) const;
+  void ordenamientoPorResiduos(uint32_t *A, uint32_t n) const;
+
+  /**
+   * @brief Retorna un std::string con los datos de la tarea.
+   * 
+   * Este método devuelve una cadena de texto que contiene el carné, nombre y tarea.
+   * 
+   * @return std::string Una cadena de texto con los datos de la tarea.
+   */
+  constexpr const char* imprimirDatosDeTarea() const {
+    return "Carné: B96804, Nombre: Jeremy Rojas, Tarea 1";
+  }
 };
